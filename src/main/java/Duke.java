8703f4290e015c7cb67a142e2a8c73ef7a37aef2
chuckiex3 <francenemooi@gmail.com>
2020-08-16 14:24:10 +0800

    created a character
    Changed Duke to my own character Toto

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo);*/
+
+        String logo = "      /-\\    /-\\ \n" //4 spaces
+                + "     /  |_9_/  |\n" //4 spaces
+                + "    /,,o  3  o,,\\ \n"; //4 spaces
+
+
+        System.out.println("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+        System.out.println("    Hullo I'm Toto!\n" + logo); //4 spaces
+        System.out.println("    How can Toto help today?"); //4 spaces
+        System.out.println("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
     }
 }
