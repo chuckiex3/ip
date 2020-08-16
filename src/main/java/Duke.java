d220200ff5169e3a5c5e957200b0e7b351e2a35a
chuckiex3 <francenemooi@gmail.com>
2020-08-16 14:41:59 +0800

    added a bye message
    Toto exits

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,14 +7,16 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);*/
 
-        String logo = "      /-\\    /-\\ \n" //4 spaces
+        String logo1 = "      /-\\    /-\\ \n" //4 spaces
                 + "     /  |_9_/  |\n" //4 spaces
                 + "    /,,o  3  o,,\\ \n"; //4 spaces
-
-
+        String logo2 = "      /-\\    /-\\ \n"
+                + "     /  |_9_/  |\n"
+                + "    /  T  ^  T \\ \n";
         System.out.println("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
-        System.out.println("    Hullo I'm Toto!\n" + logo); //4 spaces
+        System.out.println("    Hullo I'm Toto!\n" + logo1); //4 spaces
         System.out.println("    How can Toto help today?"); //4 spaces
         System.out.println("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+        System.out.println("    Bye\n    Toto will be lonely... :<\n" + logo2);
     }
 }
