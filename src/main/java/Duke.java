b14a51bbd828a2d038ddcd20e571a19a2484ef8c
chuckiex3 <francenemooi@gmail.com>
2020-08-22 22:09:08 +0800

    greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+import java.lang.String;
+
 public class Duke {
+    static final int MAX_NO = 100;
     public static void main(String[] args) {
         printGreeting();
-        printBye();
+        getMessage();
+        //printBye();
     }
 
     public static void printGreeting() {
@@ -15,14 +20,44 @@ public class Duke {
     }
 
     public static void printDivider(){
-        System.out.println("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+        System.out.println("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
     }
 
     public static void printBye(){
         String logo = "      /-\\    /-\\ \n"
             + "     /  |_9_/  |\n"
             + "    /  T  ^  T \\ \n";
+        printDivider();
         System.out.println("    Byee\n    Toto will be lonely... :<\n" + logo);
         printDivider();
+    }
+
+    private static void echoMessage(String message){
+        printDivider();
+        System.out.println(message);
+        printDivider();
+    }
+
+    private static void getMessage(){
+        Scanner in = new Scanner(System.in);
+        String[] input = new String[MAX_NO];
+        int i = 0;
+        while (true) {
+            input[i] = in.nextLine();
+            if (input[i].equals("bye") || input[i].equals("Bye")) {
+                printBye();
+                break;
+            } else {
+                echoMessage(input[i]);
+                i++;
+            }
+        }
+    }
+
+    public static int check_command(String command) {
+        if (command.equals("help") || command.equals("HELP")) {
+            return 1;
+        }
+        return 0;
     }
 }
