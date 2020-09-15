7c22ee08a779e75fa8f22bf8e5353f719b20a5a0
chuckiex3 <francenemooi@gmail.com>
2020-09-15 23:35:29 +0800

    Created Save.java to help with saving function

---
+++ src/main/java/duke/commands/Task.java
@@ -0,0 +1,31 @@
+package duke.commands;
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String toString(){
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public void setDone(){
+        isDone = true;
+    }
+
+    public boolean getStatus(){
+        if (isDone == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
